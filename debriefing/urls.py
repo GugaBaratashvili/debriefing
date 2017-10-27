300a5b1d931968503b9a1e9491cc5b4c623604dd
GugaBaratashvili <gugabaratashvili@gmail.com>
2017-10-27 13:38:36 +0300

    Update urls.py

--- debriefing/urls.py
+++ debriefing/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', post_list, name='post_list'),
     url(r'^send/$', post_send, name='post_send'),
-    url(r'^privacy/$', privacy, name='privacy'),
+    url(r'^PrivacyAct/$', privacy, name='privacy'),
 ]
