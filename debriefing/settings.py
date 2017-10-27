f9c8c4a7ff3fb946b20556085d229162fbd3c162
GugaBaratashvili <gugabaratashvili@gmail.com>
2017-10-27 14:03:41 +0300

    Update settings.py

--- debriefing/settings.py
+++ debriefing/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'na0pp5v*_h2pm-e!hc*8s4@$-u!##vhmrrrexbup@r3c$ag(zi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['dry-depths-81478.herokuapp.com']
 
